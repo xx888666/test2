569da34fbd3fa28ce8e2bff1da5e58a9aa464cc7
xx <23834390@qq.com>
2019-09-30 14:23:38 +0800

    test11111

---
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -0,0 +1,14 @@
+package com.tedu.sp01.service;
+
+import java.util.List;
+
+import com.tedu.sp01.pojo.Item;
+
+public interface ItemService {
+
+	//获取指定订单中的商品
+	List<Item> getItems(String orderId);
+	
+	//减少商品库存
+	void decreaseNumbers(List<Item> i);
+}
